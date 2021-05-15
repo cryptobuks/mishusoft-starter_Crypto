bd3fee432fda38e23f54fe2f3110bb1691d4d9a3
আল আমিন আহম্মেদ <34349365+mrabirahamed@users.noreply.github.com>
2021-05-15 14:36:28 +0600

    Update

--- Sources/Mishusoft/Ema/UrlSplitters/SystemConfiguration.php
+++ Sources/Mishusoft/Ema/UrlSplitters/SystemConfiguration.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Mishusoft\Ema\UrlSplitters;
-
 
 use Mishusoft\Framework\Chipsets\Http\Browser;
 use Mishusoft\Framework\Chipsets\System;
@@ -13,11 +11,19 @@ use ZipArchive;
 class SystemConfiguration
 {
 
+
+    /**
+     * SystemConfiguration constructor.
+     */
     public function __construct()
     {
 
-    }
+    }//end __construct()
 
+
+    /**
+     * @param array $request
+     */
     public function install(array $request)
     {
         if (strlen(file_get_contents('php://input')) > 0) {
@@ -25,63 +31,80 @@ class SystemConfiguration
             System::communicate();
             exit();
         } else {
-            Firewall::runtimeFailure("Not Found", [
-                "debug" => [
-                    "file" => ucfirst($request["controller"]),
-                    "location" => (new Browser())->getVisitedPage(),
-                    "description" => "Your requested url not found!!"],
-                "error" => ["description" => "Your requested url not found!!"]
-            ]);
+            Firewall::runtimeFailure(
+                'Not Found',
+                [
+                    'debug' => [
+                        'file'        => ucfirst($request['controller']),
+                        'location'    => (new Browser())->getVisitedPage(),
+                        'description' => 'Your requested url not found!!',
+                    ],
+                    'error' => ['description' => 'Your requested url not found!!'],
+                ]
+            );
         }
-    }
 
+    }//end install()
+
+
+    /**
+     * @param array $request
+     */
     public function update(array $request)
     {
-        if (array_key_exists("update", $_FILES)) {
-            $uploader = new Uploader($_FILES["update"]);
+        if (array_key_exists('update', $_FILES)) {
+            $uploader = new Uploader($_FILES['update']);
             if (!$uploader->file_temp_name) {
                 echo 'Please browse for a file before clicking upload button.';
                 exit();
             }
+
             if ($uploader->err_message === 1) {
                 echo "Fetching error to upload $uploader->file_name.";
                 exit();
             }
+
             if ($uploader->file_type !== 'application/zip') {
                 echo 'Please select a zip (.zip) file before clicking upload button.';
                 exit();
             }
 
-            if (file_exists(PHP_RUNTIME_SYSTEM_TEMP_PATH . $uploader->file_name)) {
+            if (file_exists(PHP_RUNTIME_SYSTEM_TEMP_PATH.$uploader->file_name) === true) {
                 echo "$uploader->file_name already exists.";
                 exit();
             }
 
-
-            if (move_uploaded_file($uploader->file_temp_name, PHP_RUNTIME_SYSTEM_TEMP_PATH . $uploader->file_name)) {
-                $filename = strtoupper(str_replace("-"," ",pathinfo($uploader->file_name, PATHINFO_FILENAME)));
-                $zip = new ZipArchive;
-                if ($zip->open(PHP_RUNTIME_SYSTEM_TEMP_PATH . $uploader->file_name) === TRUE) {
+            if (move_uploaded_file($uploader->file_temp_name, PHP_RUNTIME_SYSTEM_TEMP_PATH.$uploader->file_name)) {
+                $filename = strtoupper(str_replace('-', ' ', pathinfo($uploader->file_name, PATHINFO_FILENAME)));
+                $zip      = new ZipArchive();
+                if ($zip->open(PHP_RUNTIME_SYSTEM_TEMP_PATH.$uploader->file_name) === true) {
                     $zip->extractTo(PHP_RUNTIME_ROOT_PATH);
                     $zip->close();
-                    unlink(PHP_RUNTIME_SYSTEM_TEMP_PATH . $uploader->file_name);
-                    echo "$filename successfully installed on " . System::getAbsoluteInstalledURL();
+                    unlink(PHP_RUNTIME_SYSTEM_TEMP_PATH.$uploader->file_name);
+                    echo "$filename successfully installed on ".System::getAbsoluteInstalledURL();
                 } else {
-                    echo "$filename installation failed on " . System::getAbsoluteInstalledURL();
+                    echo "$filename installation failed on ".System::getAbsoluteInstalledURL();
                 }
-
             } else {
-                echo pathinfo($uploader->file_name, PATHINFO_BASENAME) . " upload failed.";
+                echo pathinfo($uploader->file_name, PATHINFO_BASENAME).' upload failed.';
             }
+
             exit();
         } else {
-            Firewall::runtimeFailure("Not Found", [
-                "debug" => [
-                    "file" => ucfirst($request["controller"]),
-                    "location" => (new Browser())->getVisitedPage(),
-                    "description" => "Your requested url not found!!"],
-                "error" => ["description" => "Your requested url not found!!"]
-            ]);
-        }
-    }
-}
+            Firewall::runtimeFailure(
+                'Not Found',
+                [
+                    'debug' => [
+                        'file'        => ucfirst($request['controller']),
+                        'location'    => (new Browser())->getVisitedPage(),
+                        'description' => 'Your requested url not found!!',
+                    ],
+                    'error' => ['description' => 'Your requested url not found!!'],
+                ]
+            );
+        }//end if
+
+    }//end update()
+
+
+}//end class
