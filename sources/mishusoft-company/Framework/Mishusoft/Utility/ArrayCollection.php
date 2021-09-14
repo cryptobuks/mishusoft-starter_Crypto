b393a04734a2ae7a5b9c785ea1e6271f63a28d8a
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-09-14 15:58:28 +0600

    Update ArrayCollection.php

--- sources/mishusoft-company/Framework/Mishusoft/Utility/ArrayCollection.php
+++ sources/mishusoft-company/Framework/Mishusoft/Utility/ArrayCollection.php
@@ -27,6 +27,27 @@ class ArrayCollection
     }
 
     /**
+     * @param object $object
+     * @return array
+     */
+    public static function objectToArray(object $object): array
+    {
+        $reflectionClass = new \ReflectionClass(get_class($object));
+        $array = array();
+        foreach ($reflectionClass->getProperties() as $property) {
+            $property->setAccessible(true);
+            if (is_object($property->getValue($object))) {
+                $array[$property->getName()] = self::objectToArray($property->getValue($object));
+            } else {
+                $array[$property->getName()] = $property->getValue($object);
+                $property->setAccessible(false);
+            }
+            //print_r($property->getValue($object), false);
+        }
+        return $array;
+    }
+
+    /**
      * @param array $haystack
      * @param string $key
      * @return string|array|int
