be2d212cd1195cf8d51cb9f48d14ad9042531664
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-14 23:24:28 +0600

    Create DocEnd.php

---
+++ sources/mishusoft-company/Framework/Mishusoft/Storage/FileSystem/Dallgoot/Yaml/Nodes/DocEnd.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mishusoft\Storage\FileSystem\Dallgoot\Yaml\Nodes;
+
+/**
+ *
+ * @author  Stéphane Rebai <stephane.rebai@gmail.com>
+ * @license Apache 2.0
+ * @link    https://github.com/dallgoot/yaml
+ */
+class DocEnd extends NodeGeneric
+{
+    public function build(&$parent = null)
+    {
+        // Does nothing
+        return null;
+    }
+}
