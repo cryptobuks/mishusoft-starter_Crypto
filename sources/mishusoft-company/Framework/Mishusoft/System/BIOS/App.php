5d63be24580b2a8ae0e6f4741d28a6c8bd909b5b
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-14 08:52:56 +0600

    Update App.php

--- sources/mishusoft-company/Framework/Mishusoft/System/BIOS/App.php
+++ sources/mishusoft-company/Framework/Mishusoft/System/BIOS/App.php
@@ -18,96 +18,97 @@ use Mishusoft\Utility\Debug;
 
 class App extends BIOS
 {
-
     /**
      * BIOS initialise function
-     *
-     * @throws \GeoIp2\Exception\AddressNotFoundException
-     * @throws \JsonException
-     * @throws \MaxMind\Db\Reader\InvalidDatabaseException
-     * @throws \Mishusoft\Exceptions\ErrorException
-     * @throws \Mishusoft\Exceptions\HttpException\HttpResponseException
-     * @throws \Mishusoft\Exceptions\JsonException
-     * @throws \Mishusoft\Exceptions\LogicException\InvalidArgumentException
-     * @throws \Mishusoft\Exceptions\PermissionRequiredException
-     * @throws \Mishusoft\Exceptions\RuntimeException
-     * @throws \Mishusoft\Exceptions\RuntimeException\NotFoundException
      */
     public static function initialise():void
     {
-        self::singleton(function ($registry) {
-            //Debug::preOutput($registry);
-            //Debug::preOutput('before setting data');
-            $registry->browser              = Http\Browser::getInstance();
-            $registry->requestClassic       = Http\Request\Classic::getInstance();
-            $registry->requestQualifiedAPI  = Http\Request\QualifiedAPI::getInstance();
-            $registry->ip                   = new Http\IP();
+        self::singleton(/**
+         * @throws \Mishusoft\Exceptions\PermissionRequiredException
+         * @throws \Mishusoft\Exceptions\JsonException
+         * @throws \JsonException
+         * @throws \Mishusoft\Exceptions\ErrorException
+         * @throws \Mishusoft\Exceptions\LogicException\InvalidArgumentException
+         * @throws \Mishusoft\Exceptions\RuntimeException\NotFoundException
+         * @throws \Mishusoft\Exceptions\RuntimeException
+         * @throws \GeoIp2\Exception\AddressNotFoundException
+         * @throws \MaxMind\Db\Reader\InvalidDatabaseException
+         * @throws \Mishusoft\Exceptions\HttpException\HttpResponseException
+         */
+            function ($registry) {
+                //Debug::preOutput($registry);
+                //Debug::preOutput('before setting data');
+                $registry->browser              = Http\Browser::getInstance();
+                $registry->requestClassic       = Http\Request\Classic::getInstance();
+                $registry->requestQualifiedAPI  = Http\Request\QualifiedAPI::getInstance();
+                $registry->ip                   = new Http\IP();
 
-            //Debug::preOutput('after setting data');
-            //Debug::preOutput($registry);
+                //Debug::preOutput('after setting data');
+                //Debug::preOutput($registry);
 
 
-            // Communicate with framework.
-            Log::info('Start framework application.');
-            Framework::init($registry, function ($framework) use ($registry) {
-                // Instance system memory.
-                Log::info('Start system memory.');
-                Memory::enable($framework);
+                // Communicate with framework.
+                Log::info('Start framework application.');
+                Framework::init($registry, function ($framework) use ($registry) {
+                    // Instance system memory.
+                    Log::info('Start system memory.');
+                    Memory::enable($framework);
 
-                //Logger::write('Start system cache manager.');
-                //CacheManager::start();
+                    //Logger::write('Start system cache manager.');
+                    //CacheManager::start();
 
-                Log::info('Start system firewall.');
-                $firewall = new Firewall($framework);
+                    Log::info('Start system firewall.');
+                    $firewall = new Firewall($framework);
 
-                Log::info('Firewall check access validity of client.');
-                if ($firewall->isRequestAccepted() === true) {
-                    if (Registry::Browser()->getRequestMethod() === 'OPTIONS') {
-                        $note = 'The HTTP OPTIONS method requests permitted to communicate';
-                        // add welcome note for http options method
-                        Stream::json([
-                            'message' => [
-                                'type' => 'success',
-                                'contents' => sprintf(
-                                    "%s for %s.",
-                                    $note,
-                                    $registry::Browser()::getVisitedPage()
-                                ),
-                            ],
-                        ]);
-                        Log::info(
-                            sprintf("%s for %s.", $note, $registry::Browser()::getVisitedPage()),
-                            LOG_STYLE_FULL,
-                            LOG_TYPE_ACCESS
-                        );
-                    } else {
-                        Log::info('Access validity of client has been passed.');
-                        Log::info('Start system session.');
-                        Session::init();
+                    Log::info('Firewall check access validity of client.');
+                    if ($firewall->isRequestAccepted() === true) {
+                        if (Registry::Browser()->getRequestMethod() === 'OPTIONS') {
+                            $note = 'The HTTP OPTIONS method requests permitted to communicate';
+                            // add welcome note for http options method
+                            Stream::json([
+                                'message' => [
+                                    'type' => 'success',
+                                    'contents' => sprintf(
+                                        "%s for %s.",
+                                        $note,
+                                        $registry::Browser()::getVisitedPage()
+                                    ),
+                                ],
+                            ]);
+                            Log::info(
+                                sprintf("%s for %s.", $note, $registry::Browser()::getVisitedPage()),
+                                LOG_STYLE_FULL,
+                                LOG_TYPE_ACCESS
+                            );
+                        } else {
+                            Log::info('Access validity of client has been passed.');
+                            Log::info('Start system session.');
+                            Session::init();
 
-                        if (file_exists(Storage::applicationDirectivePath())) {
-                            /*
+                            if (file_exists(Storage::applicationDirectivePath())) {
+                                /*
                              * Start special url handler [Api Url Service].
-                             */
-                            QualifiedAPI::run($registry::RequestQualifiedAPI());
+                                 */
+                                QualifiedAPI::run($registry::RequestQualifiedAPI());
 
-                            /*
+                                /*
                              * Start special url handler [Embed Mishusoft Application].
-                             */
-                            //Debug::preOutput($registry::RequestQualifiedAPI());
-                            Ema::run($registry::RequestQualifiedAPI());
+                                 */
+                                //Debug::preOutput($registry::RequestQualifiedAPI());
+                                Ema::run($registry::RequestQualifiedAPI());
+                            }
+
+
+                            //execute framework core
+                            $framework->execute();
                         }
-
-
-                        //execute framework core
-                        $framework->execute();
-                    }
-                } else {
-                    Log::error('Access validity of client has been failed.');
-                    Log::alert('Make a action against client.');
-                    $firewall->defenceActivate();
-                }//end if
-            });
-        });
+                    } else {
+                        Log::error('Access validity of client has been failed.');
+                        Log::alert('Make a action against client.');
+                        $firewall->defenceActivate();
+                    }//end if
+                });
+            }
+        );
     }//end initialise()
 }
