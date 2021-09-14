6b415558573ca36883fa4dd5651c8d7c50afec5a
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-14 23:23:12 +0600

    Update NodeFactory.php

--- sources/mishusoft-company/Framework/Mishusoft/Storage/FileSystem/Dallgoot/Yaml/NodeFactory.php
+++ sources/mishusoft-company/Framework/Mishusoft/Storage/FileSystem/Dallgoot/Yaml/NodeFactory.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace Dallgoot\Yaml;
+namespace Mishusoft\Storage\FileSystem\Dallgoot\Yaml;
 
-use Dallgoot\Yaml\Nodes;
-use Dallgoot\Yaml\Regex;
-use Dallgoot\Yaml\Nodes\NodeGeneric;
+use Mishusoft\Storage\FileSystem\Dallgoot\Yaml\Nodes;
+use Mishusoft\Storage\FileSystem\Dallgoot\Yaml\Nodes\Abstract\NodeGeneric;
 
 /**
  * Analyzes $nodeString
@@ -20,12 +19,21 @@ final class NodeFactory
 {
     private const JSON_OPTIONS = \JSON_PARTIAL_OUTPUT_ON_ERROR|\JSON_UNESCAPED_SLASHES;
 
-    final public static function get($nodeString = null, $line = 0):NodeGeneric
+    /**
+     * @throws \Exception
+     */
+    final public static function get($nodeString = null, $line = 0): Nodes\Blank
     {
         $trimmed = ltrim($nodeString);
-        if ($trimmed === '')                                return new Nodes\Blank($nodeString, $line);
-        elseif (substr($trimmed, 0, 3) === '...')           return new Nodes\DocEnd($nodeString, $line);
-        elseif ((bool) preg_match(Regex::KEY, $trimmed, $matches)) return new Nodes\Key($nodeString, $line, $matches);
+        if ($trimmed === '') {
+            return new Nodes\Blank($nodeString, $line);
+        }
+
+        if (str_starts_with($trimmed, '...')) {
+            return new Nodes\DocEnd($nodeString, $line);
+        } elseif ((bool) preg_match(Regex::KEY, $trimmed, $matches)) {
+            return new Nodes\Key($nodeString, $line, $matches);
+        }
         else {
             $first = $trimmed[0];
             $stringGroups = ['-',         '>|' ,   '"\'',    "#%" ,    "{[" ,       ":?" ,       '*&!'];
@@ -47,8 +55,9 @@ final class NodeFactory
     /**
      * Return the correct Node Object between NodeComment OR NodeDirective
      *
-     * @param      string   $nodeString  The node string
-     * @param      integer  $line         The line
+     * @param string $first
+     * @param string $nodeString The node string
+     * @param integer $line The line
      *
      * @return     NodeGeneric
      */
@@ -56,21 +65,22 @@ final class NodeFactory
     {
         if ($first === "#") {
             return new Nodes\Comment(ltrim($nodeString), $line);
-        } else {
-            if (preg_match(Regex::DIRECTIVE_TAG, $nodeString)
-                || preg_match(Regex::DIRECTIVE_VERSION, $nodeString)) {
-                return new Nodes\Directive(ltrim($nodeString), $line);
-            } else {
-                throw new \ParseError("Invalid/Unknown Directive", 1);
-            }
         }
+
+        if (preg_match(Regex::DIRECTIVE_TAG, $nodeString)
+            || preg_match(Regex::DIRECTIVE_VERSION, $nodeString)) {
+            return new Nodes\Directive(ltrim($nodeString), $line);
+        }
+
+        throw new \ParseError("Invalid/Unknown Directive", 1);
     }
 
     /**
      * Set $node type and value when $nodevalue starts with a quote (simple or double)
      *
+     * @param string $first
      * @param string $nodeString The node value
-     * @param int    $line       The line
+     * @param int $line The line
      *
      * @return     NodeGeneric
      */
@@ -83,8 +93,9 @@ final class NodeFactory
     /**
      * Set $node type and value when NodeValue starts with a Set characters "?:"
      *
+     * @param string $first
      * @param string $nodeString The node value
-     * @param int    $line       The line
+     * @param int $line The line
      *
      * @return     NodeGeneric
      */
@@ -98,31 +109,32 @@ final class NodeFactory
      * Determines the Node type and value when a compact object/array syntax is found
      *
      * @param string $nodeString The value assumed to start with { or [ or characters
-     * @param int    $line       The line
+     * @param int $line The line
      *
      * @return     NodeGeneric
+     * @throws \Exception
      */
     final private static function onCompact(string $first, string $nodeString, int $line):NodeGeneric
     {
         json_decode($nodeString, false, 512, self::JSON_OPTIONS);
         if (json_last_error() === \JSON_ERROR_NONE) {
             return new Nodes\JSON($nodeString, $line);
-        } else {
-            $backtrack_setting = "pcre.backtrack_limit";
-            ini_set($backtrack_setting, "-1");
-            $isMapping  = preg_match(Regex::MAPPING, trim($nodeString));
-            $isSequence = preg_match(Regex::SEQUENCE, trim($nodeString));
-            ini_restore($backtrack_setting);
+        }
 
-            if (is_bool($isMapping) || is_bool($isSequence)) {
-                throw new \Exception("Regex Error for ".(is_bool($isMapping) ? 'mapping:' :'sequence:').preg_last_error());
-            }
-            if ($isMapping) {
-                // var_dump(Regex::MAPPING, trim($nodeString));
-                return new Nodes\CompactMapping($nodeString, $line);
-            } elseif ($isSequence) {
-                return new Nodes\CompactSequence($nodeString, $line);
-            }
+        $backtrack_setting = "pcre.backtrack_limit";
+        ini_set($backtrack_setting, "-1");
+        $isMapping  = preg_match(Regex::MAPPING, trim($nodeString));
+        $isSequence = preg_match(Regex::SEQUENCE, trim($nodeString));
+        ini_restore($backtrack_setting);
+
+        if (is_bool($isMapping) || is_bool($isSequence)) {
+            throw new \Exception("Regex Error for ".(is_bool($isMapping) ? 'mapping:' :'sequence:').preg_last_error());
+        }
+        if ($isMapping) {
+            // var_dump(Regex::MAPPING, trim($nodeString));
+            return new Nodes\CompactMapping($nodeString, $line);
+        } elseif ($isSequence) {
+            return new Nodes\CompactSequence($nodeString, $line);
         }
         return new Nodes\Partial($nodeString, $line);
     }
@@ -131,17 +143,22 @@ final class NodeFactory
      * Determines Node type and value when an hyphen "-" is found
      *
      * @param string $nodeString The node string value
-     * @param int    $line       The line
+     * @param int $line The line
      *
      * @return     NodeGeneric
+     * @throws \Exception
      */
     final private static function onHyphen(string $first, string $nodeString, int $line):NodeGeneric
     {
-        if (substr($nodeString, 0, 3) === '---')              return new Nodes\DocStart($nodeString, $line);
-        elseif ((bool) preg_match(Regex::ITEM, ltrim($nodeString)))  return new Nodes\Item($nodeString, $line);
-        else {
-            return new Nodes\Scalar($nodeString, $line);
+        if (str_starts_with($nodeString, '---')) {
+            return new Nodes\DocStart($nodeString, $line);
         }
+
+        if ((bool) preg_match(Regex::ITEM, ltrim($nodeString))) {
+            return new Nodes\Item($nodeString, $line);
+        }
+
+        return new Nodes\Scalar($nodeString, $line);
     }
 
     /**
@@ -160,19 +177,18 @@ final class NodeFactory
         }
         if ($first === "!") {
             return new Nodes\Tag($nodeString, $line);
-        } else {
-            return new Nodes\Anchor($nodeString, $line);
         }
+
+        return new Nodes\Anchor($nodeString, $line);
     }
 
     final private static function onLiteral(string $first, string $nodeString, int $line):NodeGeneric
     {
-        switch ($first) {
-            case '>': return new Nodes\LiteralFolded($nodeString, $line);
-            case '|': return new Nodes\Literal($nodeString, $line);
-            default:
-                throw new \ParseError("Not a literal node !! '$first' on line:$line");
-        }
+        return match ($first) {
+            '>' => new Nodes\LiteralFolded($nodeString, $line),
+            '|' => new Nodes\Literal($nodeString, $line),
+            default => throw new \ParseError("Not a literal node !! '$first' on line:$line"),
+        };
     }
 
 }
