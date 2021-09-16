af19fc4a0acf3687001363c4f2e22c579f241713
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-16 23:17:13 +0600

    Update Table.php

--- sources/mishusoft-company/Framework/Mishusoft/Databases/MishusoftSQLStandalone/Table.php
+++ sources/mishusoft-company/Framework/Mishusoft/Databases/MishusoftSQLStandalone/Table.php
@@ -2,10 +2,12 @@
 
 namespace Mishusoft\Databases\MishusoftSQLStandalone;
 
-use Mishusoft\Databases\MishusoftSQLStandalone as MishusoftQL;
 use Mishusoft\Exceptions\DbException;
 use Mishusoft\Exceptions\RuntimeException;
 use Mishusoft\Http;
+use Mishusoft\Storage\FileSystem;
+use Mishusoft\Utility\ArrayCollection;
+use Mishusoft\Utility\Implement;
 
 class Table extends CommonDependency implements TableInterface
 {
@@ -19,7 +21,7 @@ class Table extends CommonDependency implements TableInterface
     public function __construct(
         private string $database
     ) {
-        parent::__construct(MishusoftQL::WHO_AM_I, MishusoftQL::DB_FILE_FORMAT);
+        parent::__construct(CommonStructureInterface::WHO_AM_I, CommonStructureInterface::DB_FILE_FORMAT);
         $this->setCurrentDatabase($this->database);
         $this->dataDirectory = $this->directory($this->database);
 
@@ -91,7 +93,7 @@ class Table extends CommonDependency implements TableInterface
     public function read(string $table_name): DataInterface
     {
         if (in_array($table_name, $this->tablesAll, true)) {
-            return new Data($table_name);
+            return new Data(dirname($this->tableFile($table_name)), $table_name);
         }
 
         throw new DbException("Unknown table ($table_name).");
@@ -101,107 +103,110 @@ class Table extends CommonDependency implements TableInterface
      * @param string $old_name
      * @param string $new_name
      * @return mixed
+     * @throws DbException
      */
     public function rename(string $old_name, string $new_name): bool
     {
         if (in_array($old_name, $this->tablesAll, true)) {
             if (file_exists($this->tableFile($old_name))) {
                 $contents = $this->databaseProperties;
-                if (array_key_exists("tables", $contents)) {
-                    if (!in_array($new_name, $contents["tables"], true)) {
-                        unset($contents["tables"][array_search($old_name, $contents["tables"], true)]);
-                        array_push($contents["tables"], $new_name);
-                    }
+                if (array_key_exists("tables", $contents)
+                    && !in_array($new_name, $contents["tables"], true)) {
+                    unset($contents["tables"][array_search($old_name, $contents["tables"], true)]);
+                    $contents["tables"][] = $new_name;
                 }
                 //sort tables
                 $contents["tables"] = $this->sort($properties["tables"]);
-                \Mishusoft\Storage\FileSystem::readFile($this->databaseFile($this->database), function ($contents) use ($new_name, $old_name) {
-                    $contents = json_decode($contents, true);
-                    if (_Array::value($contents, "tables")) {
-                        if (!in_array($new_name, $contents["tables"])) {
-                            unset($contents["tables"][array_search($old_name, $contents["tables"])]);
-                            array_push($contents["tables"], $new_name);
-                        }
-                    }
-                    $tables = [];
-                    foreach ($contents["tables"] as $table) {
-                        array_push($tables, $table);
-                    }
-                    $contents["tables"] = $tables;
+                $contents = (array) Implement::jsonDecode(
+                    FileSystem::read($this->databaseFile($this->database)),
+                    IMPLEMENT_JSON_IN_ARR
+                );
+                if (ArrayCollection::value($contents, "tables")
+                    && !in_array($new_name, $contents["tables"], true)) {
+                    unset($contents["tables"][array_search($old_name, $contents["tables"], true)]);
+                    $contents["tables"][] = $new_name;
+                }
+                $tables = [];
+                foreach ($contents["tables"] as $table) {
+                    $tables[] = $table;
+                }
+                $contents["tables"] = $tables;
 
-                    array_multisort($contents["tables"], SORT_ASC);
-                    ksort($contents["tables"], SORT_ASC);
-                    \Mishusoft\Storage\FileSystem::saveToFile($this->databaseFile($this->database), json_encode($contents));
-                });
+                array_multisort($contents["tables"], SORT_ASC);
+                ksort($contents["tables"], SORT_ASC);
+                FileSystem::saveToFile($this->databaseFile($this->database), Implement::toJson($contents));
                 rename(
-                    join(DIRECTORY_SEPARATOR, [$this->dataDirectory, $old_name . self::dbTableFileFormat]),
-                    join(DIRECTORY_SEPARATOR, [$this->dataDirectory, $new_name . self::dbTableFileFormat])
+                    implode(DS, [$this->dataDirectory, $old_name . self::DB_TABLE_FILE_FORMAT]),
+                    implode(DS, [$this->dataDirectory, $new_name . self::DB_TABLE_FILE_FORMAT])
                 );
                 return true;
-            } else {
-                MishusoftSQLStandalone::error(404, "Table ($old_name)'s data file is not exists.");
-                exit();
             }
-        } else {
-            MishusoftSQLStandalone::error(404, "Table ($old_name) is not exists.");
-            exit();
+
+            throw new DbException("Table ($old_name)'s data file is not exists");
         }
+
+        throw new DbException("Table ($old_name) is not exists");
     }
 
     /**
-     * @param $table_name
-     * @return false|void
+     * @param array|string $name
+     * @return bool
      */
-    public function delete($table_name): bool
+    public function delete(array|string $name): bool
     {
-        if (is_array($table_name)) {
-            foreach ($table_name as $tbl) {
+        if (is_array($name)) {
+            foreach ($name as $tbl) {
                 $this->deleteTable($tbl);
             }
         } else {
-            $this->deleteTable($table_name);
+            $this->deleteTable($name);
         }
     }
 
     /**
      * @param string $table_name
+     * @throws DbException
      */
-    private function deleteTable(string $table_name)
+    private function deleteTable(string $table_name): void
     {
-        if (in_array($table_name, $this->table_all)) {
-            \Mishusoft\Storage\FileSystem::readFile($this->databaseFile($this->database), function ($contents) use ($table_name) {
-                $contents = json_decode($contents, true);
-                if (ArrayCollection::value($contents, "tables")) {
-                    unset($contents["tables"][array_search($table_name, $contents["tables"])]);
-                }
-                array_multisort($contents["tables"], SORT_ASC);
-                ksort($contents["tables"], SORT_ASC);
-                \Mishusoft\Storage\FileSystem::saveToFile($this->databaseFile($this->database), json_encode($contents));
-            });
-
-            \Mishusoft\Storage\FileSystem::unlink(join(DIRECTORY_SEPARATOR, [$this->dataDirectory, $table_name . self::dbTableFileFormat]));
-        } else {
-            MishusoftSQLStandalone::error(Http::NOT_FOUND, "Table ($table_name) is not exists.");
-            exit();
-        }
-    }
-
-    public function update()
-    {
-        \Mishusoft\Storage\FileSystem::readFile($this->databaseFile($this->database), function ($contents) {
-            $contents = json_decode($contents, true);
+        if (in_array($table_name, $this->tablesAll, true)) {
+            $contents = Implement::jsonDecode(
+                FileSystem::read($this->databaseFile($this->database)),
+                IMPLEMENT_JSON_IN_ARR
+            );
             if (ArrayCollection::value($contents, "tables")) {
-                foreach (\Mishusoft\Storage\FileSystem::getDirectoryList($this->dataDirectory) as $table_name) {
-                    if (!in_array(pathinfo($table_name, PATHINFO_FILENAME), $contents["tables"], true)) {
-                        array_push($contents["tables"], pathinfo($table_name, PATHINFO_FILENAME));
-                    }
-                }
+                unset($contents["tables"][array_search($table_name, $contents["tables"], true)]);
             }
             array_multisort($contents["tables"], SORT_ASC);
             ksort($contents["tables"], SORT_ASC);
-            $this->table_all = $contents["tables"];
-            \Mishusoft\Storage\FileSystem::saveToFile($this->databaseFile($this->database), json_encode($contents));
-        });
+            FileSystem::saveToFile($this->databaseFile($this->database), Implement::toJson($contents));
+
+            FileSystem::remove(implode(DS, [$this->dataDirectory, $table_name . self::DB_TABLE_FILE_FORMAT]));
+        } else {
+            throw new DbException("Table ($table_name) is not exists");
+        }
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public function update(array $options):bool
+    {
+        $contents = (array) Implement::jsonDecode(
+            FileSystem::read($this->databaseFile($this->database)),
+            IMPLEMENT_JSON_IN_ARR
+        );
+        if (ArrayCollection::value($contents, "tables")) {
+            foreach (FileSystem::list($this->dataDirectory, 'directory') as $table_name) {
+                if (!in_array(pathinfo($table_name, PATHINFO_FILENAME), $contents["tables"], true)) {
+                    $contents["tables"][] = pathinfo($table_name, PATHINFO_FILENAME);
+                }
+            }
+        }
+        array_multisort($contents["tables"], SORT_ASC);
+        ksort($contents["tables"], SORT_ASC);
+        $this->tablesAll = $contents["tables"];
+        return (bool) FileSystem::saveToFile($this->databaseFile($this->database), Implement::toJson($contents));
     }
 
     public function __destruct()
