1af42c4ee538d6d240c9b709cfeae4eb1c674ecd
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-16 08:12:47 +0600

    Update Inflect.php

--- sources/mishusoft-company/Framework/Mishusoft/Utility/Inflect.php
+++ sources/mishusoft-company/Framework/Mishusoft/Utility/Inflect.php
@@ -308,4 +308,109 @@ class Inflect
         // either ' or " whichever is found
         return preg_replace('~^[\'"]?(.*?)[\'"]?$~', '$1', self::removeTags($content));
     }
+
+
+
+    /**
+     * convert a string from one UTF-16 char to one UTF-8 char
+     *
+     * Normally should be handled by mb_convert_encoding, but
+     * provides a slower PHP-only method for installations
+     * that lack the multibyte string extension.
+     *
+     * @param   string $utf16  UTF-16 character
+     * @return   string  UTF-8 character
+     * @access   private
+     */
+    public static function utf162utf8(string $utf16): string
+    {
+        // oh, please
+        if (function_exists('mb_convert_encoding')) {
+            return mb_convert_encoding($utf16, 'UTF-8', 'UTF-16');
+        }
+
+        $bytes = (ord($utf16[0]) << 8) | ord($utf16[1]);
+
+        switch (true) {
+            case ((0x7F & $bytes) === $bytes):
+                // this case should never be reached, because we are in ASCII range
+                // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                return chr(0x7F & $bytes);
+
+            case (0x07FF & $bytes) === $bytes:
+                // return a 2-byte UTF-8 character
+                // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                return chr(0xC0 | (($bytes >> 6) & 0x1F))
+                    . chr(0x80 | ($bytes & 0x3F));
+
+            case (0xFFFF & $bytes) === $bytes:
+                // return a 3-byte UTF-8 character
+                // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                return chr(0xE0 | (($bytes >> 12) & 0x0F))
+                    . chr(0x80 | (($bytes >> 6) & 0x3F))
+                    . chr(0x80 | ($bytes & 0x3F));
+        }
+
+        // ignoring UTF-32 for now, sorry
+        return '';
+    }
+
+
+    /**
+     * convert a string from one UTF-8 char to one UTF-16 char
+     *
+     * Normally should be handled by mb_convert_encoding, but
+     * provides a slower PHP-only method for installations
+     * that lack the multibye string extension.
+     *
+     * @param   string $utf8   UTF-8 character
+     * @return   string  UTF-16 character
+     * @access   private
+     */
+    public static function utf82utf16(string $utf8): string
+    {
+        // oh, please
+        if (function_exists('mb_convert_encoding')) {
+            return mb_convert_encoding($utf8, 'UTF-16', 'UTF-8');
+        }
+
+        switch (self::strlen8($utf8)) {
+            case 1:
+                // this case should never be reached, because we are in ASCII range
+                // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                return $utf8;
+
+            case 2:
+                // return a UTF-16 character from a 2-byte UTF-8 char
+                // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                return chr(0x07 & (ord($utf8[0]) >> 2))
+                    . chr((0xC0 & (ord($utf8[0]) << 6))
+                        | (0x3F & ord($utf8[1])));
+
+            case 3:
+                // return a UTF-16 character from a 3-byte UTF-8 char
+                // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                return chr((0xF0 & (ord($utf8[0]) << 4))
+                        | (0x0F & (ord($utf8[1]) >> 2)))
+                    . chr((0xC0 & (ord($utf8[1]) << 6))
+                        | (0x7F & ord($utf8[2])));
+        }
+
+        // ignoring UTF-32 for now, sorry
+        return '';
+    }
+
+
+    /**
+     * Calculates length of string in bytes
+     * @param string
+     * @return integer length
+     */
+    public static function strlen8(string $str): int
+    {
+        if (function_exists('mb_strlen')) {
+            return mb_strlen($str, "8bit");
+        }
+        return strlen($str);
+    }
 }
