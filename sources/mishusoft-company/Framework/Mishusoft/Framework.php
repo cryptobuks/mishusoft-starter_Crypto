395c44de4ff47e06010b3230a00767618f319eb5
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-16 08:13:02 +0600

    Update Framework.php

--- sources/mishusoft-company/Framework/Mishusoft/Framework.php
+++ sources/mishusoft-company/Framework/Mishusoft/Framework.php
@@ -64,29 +64,48 @@ class Framework extends Base
         //['test1'=>['test1'=>'test1value', 'test2'=>['test2key'=>'test2keyvalue']],
         // 'test2'=>['test1'=>'test1value', 'test2'=>['test2key'=>'test2keyvalue']]]
 
-        $array = ['test','test1','test2','test3', 5];
+        $array = ['test','test1','test2','test3', 5, "▇"];
         $array2 = ['test','test1','test2','test3'=>['test','test1','test2','test3'],'test4'=>true];
         $array3 = ['test1'=>['test1'=>'test1value', 'test2'=>['test2key'=>'test2keyvalue']],
-            'test2'=>['test1'=>'test1value', 'test2'=>['test2key'=>'CreateNewDoc()']]];
+            'test2'=>['test1'=>'test1value', 'test2'=>['test2key'=>'CreateNewDoc()']], ];
         $array4 = ['test1'=>['test1'=>'test1value', 'test2'=>['test2key'=>'test2keyvalue']],
-            'test2'=>['test1'=>'test1value', 'test2'=>['test2key'=>'http://localhost:8080']]];
+            'test2'=>['test1'=>'test1value', 'test2'=>['test2key'=>'http://localhost:8080']], ];
         $array5 = ['test1'=>['test1'=>'test1value', 'test2'=>['test2key'=>-122.6438]],
-            'test2'=>['test1'=>0.54264448532277, 'test2'=>['test2key'=>'http://localhost:8080']]];
+            'test2'=>['test1'=>0.54264448532277, 'test2'=>['test2key'=>'http://localhost:8080']], ];
         $array6 = [['test1'=>['test1'=>'test1value', 'test2'=>['test2key'=>-122.6438]],
-            'test2'=>['test1'=>0.54264448532277, 'test2'=>['test2key'=>'http://localhost:8080']]]];
+            'test2'=>['test1'=>0.54264448532277, 'test2'=>['test2key'=>'http://localhost:8080']], ]];
 
+        Debug::preOutput('Encoding');
+        Debug::preOutput('Decoding');
         Debug::preOutput(json_encode($array));
         Debug::preOutput(Implement::arrayToJson($array));
+        Debug::preOutput(json_decode(json_encode($array)));
+        Debug::preOutput(Implement::jsonToArray(Implement::arrayToJson($array)));
+
         Debug::preOutput(json_encode($array2));
         Debug::preOutput(Implement::arrayToJson($array2));
+        Debug::preOutput(json_decode(json_encode($array2)));
+        Debug::preOutput(Implement::jsonToArray(Implement::arrayToJson($array2)));
+
         Debug::preOutput(json_encode($array3));
         Debug::preOutput(Implement::arrayToJson($array3));
+        Debug::preOutput(json_decode(json_encode($array3)));
+        Debug::preOutput(Implement::jsonToArray(Implement::arrayToJson($array3)));
+
         Debug::preOutput(json_encode($array4));
         Debug::preOutput(Implement::arrayToJson($array4));
+        Debug::preOutput(json_decode(json_encode($array4)));
+        Debug::preOutput(Implement::jsonToArray(Implement::arrayToJson($array4)));
+
         Debug::preOutput(json_encode($array5));
         Debug::preOutput(Implement::arrayToJson($array5));
+        Debug::preOutput(json_decode(json_encode($array5)));
+        Debug::preOutput(Implement::jsonToArray(Implement::arrayToJson($array5)));
+
         Debug::preOutput(json_encode($array6));
         Debug::preOutput(Implement::arrayToJson($array6));
+        Debug::preOutput(json_decode(json_encode($array6)));
+        Debug::preOutput(Implement::jsonToArray(Implement::arrayToJson($array6)));
 
         Debug::preOutput(FileSystem\Dallgoot\Yaml\Yaml::parseFile(self::installFile()));
         $instance = new static();
