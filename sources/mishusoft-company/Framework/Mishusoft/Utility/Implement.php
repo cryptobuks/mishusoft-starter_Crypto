18a2318cc9293292e0920ee5189480c6102dd9f0
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-16 08:36:18 +0600

    Update Implement.php

--- sources/mishusoft-company/Framework/Mishusoft/Utility/Implement.php
+++ sources/mishusoft-company/Framework/Mishusoft/Utility/Implement.php
@@ -48,6 +48,246 @@ class Implement
     //end of make array to object
 
     //start of make array to json
+
+    public static function toJson(mixed $content) : mixed
+    {
+        return self::withLocale(function () use ($content) {
+            //Debug::preOutput(Inflect::utf162utf8($json));
+            Debug::preOutput($content);
+        });
+    }
+
+    private static function toJsonBuilder(mixed $content) : mixed
+    {
+        switch (gettype($content)) {
+            case 'boolean':
+                return $content ? 'true' : 'false';
+
+            case 'NULL':
+                return 'null';
+
+            case 'integer':
+                return (int) $content;
+
+            case 'double':
+            case 'float':
+                return  (float) $content;
+
+            case 'string':
+                // STRINGS ARE EXPECTED TO BE IN ASCII OR UTF-8 FORMAT
+                $ascii = '';
+                $strlen_var = Inflect::strlen8($content);
+
+                /*
+                 * Iterate over every character in the string,
+                 * escaping with a slash or encoding to UTF-8 where necessary
+                 */
+                for ($c = 0; $c < $strlen_var; ++$c) {
+
+                    $ord_var_c = ord($content[$c]);
+
+                    switch (true) {
+                        case $ord_var_c === 0x08:
+                            $ascii .= '\b';
+                            break;
+                        case $ord_var_c === 0x09:
+                            $ascii .= '\t';
+                            break;
+                        case $ord_var_c === 0x0A:
+                            $ascii .= '\n';
+                            break;
+                        case $ord_var_c === 0x0C:
+                            $ascii .= '\f';
+                            break;
+                        case $ord_var_c === 0x0D:
+                            $ascii .= '\r';
+                            break;
+
+                        case $ord_var_c === 0x22:
+                        case $ord_var_c === 0x2F:
+                        case $ord_var_c === 0x5C:
+                            // double quote, slash, slosh
+                            $ascii .= '\\'.$content[$c];
+                            break;
+
+                        case (($ord_var_c >= 0x20) && ($ord_var_c <= 0x7F)):
+                            // characters U-00000000 - U-0000007F (same as ASCII)
+                            $ascii .= $content[$c];
+                            break;
+
+                        case (($ord_var_c & 0xE0) === 0xC0):
+                            // characters U-00000080 - U-000007FF, mask 110XXXXX
+                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                            if ($c+1 >= $strlen_var) {
+                                ++$c;
+                                $ascii .= '?';
+                                break;
+                            }
+
+                            $char = pack('C*', $ord_var_c, ord($content[$c + 1]));
+                            ++$c;
+                            $utf16 = Inflect::utf82utf16($char);
+                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
+                            break;
+
+                        case (($ord_var_c & 0xF0) === 0xE0):
+                            if ($c+2 >= $strlen_var) {
+                                $c += 2;
+                                $ascii .= '?';
+                                break;
+                            }
+                            // characters U-00000800 - U-0000FFFF, mask 1110XXXX
+                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                            $char = pack('C*', $ord_var_c,
+                                @ord($content[$c + 1]),
+                                @ord($content[$c + 2]));
+                            $c += 2;
+                            $utf16 = Inflect::utf82utf16($char);
+                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
+                            break;
+
+                        case (($ord_var_c & 0xF8) === 0xF0):
+                            if ($c+3 >= $strlen_var) {
+                                $c += 3;
+                                $ascii .= '?';
+                                break;
+                            }
+                            // characters U-00010000 - U-001FFFFF, mask 11110XXX
+                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                            $char = pack('C*', $ord_var_c,
+                                ord($content[$c + 1]),
+                                ord($content[$c + 2]),
+                                ord($content[$c + 3]));
+                            $c += 3;
+                            $utf16 = Inflect::utf82utf16($char);
+                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
+                            break;
+
+                        case (($ord_var_c & 0xFC) === 0xF8):
+                            // characters U-00200000 - U-03FFFFFF, mask 111110XX
+                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                            if ($c+4 >= $strlen_var) {
+                                $c += 4;
+                                $ascii .= '?';
+                                break;
+                            }
+                            $char = pack('C*', $ord_var_c,
+                                ord($content[$c + 1]),
+                                ord($content[$c + 2]),
+                                ord($content[$c + 3]),
+                                ord($content[$c + 4]));
+                            $c += 4;
+                            $utf16 = Inflect::utf82utf16($char);
+                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
+                            break;
+
+                        case (($ord_var_c & 0xFE) === 0xFC):
+                            if ($c+5 >= $strlen_var) {
+                                $c += 5;
+                                $ascii .= '?';
+                                break;
+                            }
+                            // characters U-04000000 - U-7FFFFFFF, mask 1111110X
+                            // see http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+                            $char = pack('C*', $ord_var_c,
+                                ord($content{$c + 1}),
+                                ord($content{$c + 2}),
+                                ord($content{$c + 3}),
+                                ord($content{$c + 4}),
+                                ord($content{$c + 5}));
+                            $c += 5;
+                            $utf16 = Inflect::utf82utf16($char);
+                            $ascii .= sprintf('\u%04s', bin2hex($utf16));
+                            break;
+                    }
+                }
+                return  '"'.$ascii.'"';
+
+            case 'array':
+                /*
+                 * As per JSON spec if any array key is not an integer
+                 * we must treat the the whole array as an object. We
+                 * also try to catch a sparsely populated associative
+                 * array with numeric keys here because some JS engines
+                 * will create an array with empty indexes up to
+                 * max_index which can cause memory issues and because
+                 * the keys, which may be relevant, will be remapped
+                 * otherwise.
+                 *
+                 * As per the ECMA and JSON specification an object may
+                 * have any string as a property. Unfortunately due to
+                 * a hole in the ECMA specification if the key is a
+                 * ECMA reserved word or starts with a digit the
+                 * parameter is only accessible using ECMAScript's
+                 * bracket notation.
+                 */
+
+                // treat as a JSON object
+                if (is_array($content) && count($content) && (array_keys($content) !== range(0, sizeof($content) - 1))) {
+                    $properties = array_map(array($this, 'name_value'),
+                        array_keys($content),
+                        array_values($content));
+
+                    foreach($properties as $property) {
+                        if(Services_JSON::isError($property)) {
+                            return $property;
+                        }
+                    }
+
+                    return '{' . implode(',', $properties) . '}';
+                }
+
+                // treat it like a regular array
+                $elements = array_map(array($this, '_encode'), $content);
+
+                foreach($elements as $element) {
+                    if(Services_JSON::isError($element)) {
+                        return $element;
+                    }
+                }
+
+                return '[' . join(',', $elements) . ']';
+
+            case 'object':
+
+                // support toJSON methods.
+                if (($this->use & SERVICES_JSON_USE_TO_JSON) && method_exists($content, 'toJSON')) {
+                    // this may end up allowing unlimited recursion
+                    // so we check the return value to make sure it's not got the same method.
+                    $recode = $var->toJSON();
+
+                    if (method_exists($recode, 'toJSON')) {
+
+                        return ($this->use & SERVICES_JSON_SUPPRESS_ERRORS)
+                            ? 'null'
+                            : new Services_JSON_Error(class_name($var).
+                                " toJSON returned an object with a toJSON method.");
+
+                    }
+
+                    return $this->_encode( $recode );
+                }
+
+                $vars = get_object_vars($content);
+
+                $properties = array_map(array($this, 'name_value'),
+                    array_keys($vars),
+                    array_values($vars));
+
+                foreach($properties as $property) {
+                    if(Services_JSON::isError($property)) {
+                        return $property;
+                    }
+                }
+
+                return '{' . join(',', $properties) . '}';
+
+            default:
+                return ($this->use & SERVICES_JSON_SUPPRESS_ERRORS)
+                    ? 'null'
+                    : new Services_JSON_Error(gettype($content)." can not be encoded as JSON string");
+        }
+    }
     /**
      * @param array $array
      * @return string|array
