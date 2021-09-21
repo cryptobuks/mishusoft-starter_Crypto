356dae5559e811b6ff63828d68a09838c9bf2978
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-22 03:08:34 +0600

    Update FirewallBase.php

--- sources/mishusoft-company/Framework/Mishusoft/System/Firewall/FirewallBase.php
+++ sources/mishusoft-company/Framework/Mishusoft/System/Firewall/FirewallBase.php
@@ -16,6 +16,7 @@ use Mishusoft\System\Time;
 abstract class FirewallBase extends Base
 {
     use Config;
+    use Assets;
 
     /**
      * Firewall configuration array.
@@ -84,51 +85,6 @@ abstract class FirewallBase extends Base
      * @var integer
      */
     protected int $lastVisitDuration = 0;
-
-
-    /**
-     * @return string
-     */
-    protected static function logDirectory(): string
-    {
-        //data-drive/Firewall/logs/
-        return sprintf(
-            '%1$s%3$s%2$s',
-            'Firewall',
-            Time::todayDateOnly(),
-            DS
-        );
-    }
-
-    /**
-     * @param string $underTakenAction
-     * @return string
-     */
-    protected static function logFile(string $underTakenAction): string
-    {
-        //logs/Firewall/date/blocked.yml
-        //logs/Firewall/date/banned.yml
-        //logs/Firewall/date/granted.yml
-        return self::dFile(self::logDataFile(self::logDirectory(), $underTakenAction));
-    }
-
-
-    /**
-     * @return string
-     */
-    protected static function configFile(): string
-    {
-        return self::dFile(self::configDataFile('Firewall', 'config'));
-    }
-
-
-    /**
-     * @return string
-     */
-    protected static function siteFile(): string
-    {
-        return self::dFile(self::configDataFile('Firewall', 'sites'));
-    }
 
 
     /**
