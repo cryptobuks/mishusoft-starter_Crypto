3174d87e1c72bfd0556de243552afc39b84f3203
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-06 00:10:39 +0600

    Update AccountUrlHandler.php

--- sources/mishusoft-company/App/Packages/Ema/Modules/Main/UrlHandlers/AccountUrlHandler.php
+++ sources/mishusoft-company/App/Packages/Ema/Modules/Main/UrlHandlers/AccountUrlHandler.php
@@ -6,6 +6,15 @@ namespace App\Ema\Mishusoft\Main\UrlHandlers;
 use Mishusoft\Drivers\UrlHandler;
 class AccountUrlHandler extends UrlHandler
 {
+    /**
+     * @throws \Mishusoft\Exceptions\RuntimeException
+     * @throws \JsonException
+     * @throws \Mishusoft\Exceptions\ErrorException
+     * @throws \Mishusoft\Exceptions\LogicException\InvalidArgumentException
+     * @throws \Mishusoft\Exceptions\RuntimeException\NotFoundException
+     * @throws \Mishusoft\Exceptions\PermissionRequiredException
+     * @throws \Mishusoft\Exceptions\JsonException
+     */
     public function response(array $prediction):void
     {
         // TODO: Implement Response() method.
