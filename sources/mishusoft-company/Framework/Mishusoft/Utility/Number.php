433b4854ab9ce42863a22ca20a795a96b86f69a8
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-11 18:33:29 +0600

    Update Number.php

--- sources/mishusoft-company/Framework/Mishusoft/Utility/Number.php
+++ sources/mishusoft-company/Framework/Mishusoft/Utility/Number.php
@@ -14,55 +14,42 @@ class Number
      */
     public static function isPrime($number): bool
     {
-        if (($number % 2) != 0) {
+        if (($number % 2) !== 0) {
             return true;
         }
 
         for ($i = 0; $i < $number; $i++) {
             // A cheap check to see if $i is even
-            if (($i & 1) == 0) {
+            if (($i & 1) === 0) {
                 continue;
             }
 
-            if (($number % $i) == 0) {
+            if (($number % $i) === 0) {
                 return false;
             }
         }
 
         return true;
-
     }//end isPrime()
 
 
     /**
-     * @param  $number
-     * @return false|integer|string
+     * @param int $number
+     * @return integer
      */
-    public static function next($number)
+    public static function next(int $number): int
     {
-        if (is_numeric($number)) {
-            return ($number + 1);
-        }
-
-        echo 'Invalid number.';
-        return false;
-
+        return ($number + 1);
     }//end next()
 
 
     /**
-     * @param  $number
-     * @return false|integer|string
+     * @param int $number
+     * @return int
      */
-    public static function previous($number)
+    public static function previous(int $number): int
     {
-        if (is_numeric($number)) {
-            return ($number - 1);
-        }
-
-        echo 'Invalid number.';
-        return false;
-
+        return ($number - 1);
     }//end previous()
 
 
@@ -70,14 +57,14 @@ class Number
      * Format a number with grouped thousands
      *
      * @link   https://php.net/manual/en/function.number-format.php
-     * @param  float   $number        <p>
+     * @param float $number <p>
      *          The number being formatted.
      *          </p>
-     * @param  integer $decimals      [optional] <p>
+     * @param integer $decimals [optional] <p>
      *          Sets the number of decimal points.
      *          </p>
-     * @param  string  $dec_point     [optional]
-     * @param  string  $thousands_sep [optional]
+     * @param string $dec_point [optional]
+     * @param string $thousands_sep [optional]
      * @return string A formatted version of number.
      */
     public static function format(float $number, int $decimals, string $dec_point, string $thousands_sep): string
@@ -89,29 +76,23 @@ class Number
             result:: 1234.57
         */
         return number_format($number, $decimals, $dec_point, $thousands_sep);
-
     }//end format()
 
 
     /**
-     * @param  integer $int
+     * @param integer $int
      * @return integer
      */
     public static function filterInt(int $int): int
     {
-        if (is_int($int) === true) {
-            return $int;
-        }
-
-        return 0;
-
+        return $int;
     }//end filterInt()
 
 
     /**
      * @param  $value
-     * @param  array $arraylist
-     * @return integer|mixed
+     * @param array $arraylist
+     * @return int
      */
     public static function getInt($value, array $arraylist): int
     {
@@ -121,14 +102,10 @@ class Number
         }
 
         return 0;
-
     }//end getInt()
 
 
-    function __destruct()
+    public function __destruct()
     {
-
     }//end __destruct()
-
-
 }//end class
