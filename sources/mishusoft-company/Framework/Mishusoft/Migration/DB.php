f73a03c0f64a1b8fbd12f38189d4a7ff7431c138
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-12 08:16:10 +0600

    Update DB.php

--- sources/mishusoft-company/Framework/Mishusoft/Migration/DB.php
+++ sources/mishusoft-company/Framework/Mishusoft/Migration/DB.php
@@ -6,6 +6,6 @@ namespace Mishusoft\Migration;
 class DB
 {
     public const NAME = DB_DEFAULT_NAME;
-    public const USER = DEFAULT_OPERATING_SYSTEM_USER;
-    public const PASSWORD = DEFAULT_OPERATING_SYSTEM_PASSWORD;
+    public const USER = DB_USER_NAME;
+    public const PASSWORD = DB_USER_PASSWORD;
 }
