9d97c5a2b3dc6b13484638d1ec25fe88050bb785
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-12 21:54:53 +0600

    Update functions.php

--- bootstrap/functions.php
+++ bootstrap/functions.php
@@ -9,3 +9,18 @@ function error(string $message, int $error_level = E_USER_NOTICE)
         $error_level
     );
 }
+
+function frameworkPath(): string
+{
+    if (defined('FRAMEWORK_PATH') && file_exists(FRAMEWORK_PATH)) {
+        //set framework path from install directory
+        $frameworkPath = FRAMEWORK_PATH;
+    } elseif (defined('SRC_FRAMEWORK_PATH') && file_exists(SRC_FRAMEWORK_PATH)) {
+        //set framework path from sources
+        $frameworkPath = SRC_FRAMEWORK_PATH;
+    } else {
+        //set default framework path
+        $frameworkPath = realpath(dirname(__DIR__, 2) . 'Framework') . DS;
+    }
+    return $frameworkPath;
+}
