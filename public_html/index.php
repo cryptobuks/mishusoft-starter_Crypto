8fa5ab25bf5da6e3ada6e237b532d68ba83aff9e
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-12 08:15:38 +0600

    fetch maintenance mode

--- public_html/index.php
+++ public_html/index.php
@@ -10,6 +10,24 @@
 
 declare(strict_types=1);
 
+$getFile = static function ($name) {
+    return sprintf(
+        '%1$s%5$s%2$s%5$s%3$s%5$s%4$s',
+        realpath(dirname(__DIR__)),
+        'storages',
+        'framework',
+        $name,
+        DIRECTORY_SEPARATOR
+    );
+};
+
+//fetch maintenance mode
+if (file_exists($getFile('maintenance.php'))) {
+    include_once $getFile('resolver.php');
+    include_once $getFile('maintenance.php');
+    exit(0);
+}
+
 include_once dirname(__DIR__).'/bootstrap/app.php';
 
 Mishusoft\System\Log::info(sprintf('%s application started', __NAMESPACE__));
