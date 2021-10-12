440d8067544293982f5667a615c6a79a6b3aa847
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-12 19:11:12 +0600

    Update EmbeddedView.php

--- sources/mishusoft-company/Framework/Mishusoft/Ui/EmbeddedView.php
+++ sources/mishusoft-company/Framework/Mishusoft/Ui/EmbeddedView.php
@@ -118,20 +118,11 @@ class EmbeddedView
 
         Ui::elementList(Ui::getDocumentHeadElement(), [
             'link' => Storage::assignableWebFavicons(),
-            /*'style' => [
+            'style' => [
                 ['text' => Storage\FileSystem::read(Storage::fViewsFullPath('css/embedded.css')),],
-            ],*/
-        ]);
-
-        Ui::elementList(Ui::getDocumentHeadElement(), [
-            'link' => [
-                [
-                    'rel'=>'stylesheet',
-                    'type'=>'text/css',
-                    'href' => Storage::fViewsFullPath('css/embedded.css', 'remote')
-                ],
             ],
         ]);
+
 
         //<link rel="icon" type="image/vnd.microsoft.icon" sizes="16x16" href="{logoFolder}favicon.ico">
         //Ui::element(Ui::getDocumentHeadElement(), 'style', ['text'=>$cssContent]);
@@ -372,7 +363,7 @@ class EmbeddedView
                         );
                     }
                     if ($type === 'warning') {
-                        $additional = $additionalCommon . 'background: #f9d8b1;color: rgba(157, 87, 3, 1); border:2px solid rgba(157, 87, 3, 1);';
+                        $additional = $additionalCommon . 'background: rgba(157, 87, 3, .15);color: rgba(157, 87, 3, 1); border:2px solid rgba(157, 87, 3, 1);';
                         $contents['div'][] = self::setAttributes(
                             ['class' => $commonMessage, 'style' => $additional, 'text' => $message,]
                         );
