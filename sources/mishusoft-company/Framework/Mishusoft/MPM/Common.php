3d4487d30d3e1993aac346d4d024fc0810b443a8
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-15 05:05:38 +0600

    Update Common.php

--- sources/mishusoft-company/Framework/Mishusoft/MPM/Common.php
+++ sources/mishusoft-company/Framework/Mishusoft/MPM/Common.php
@@ -4,9 +4,7 @@ namespace Mishusoft\MPM;
 
 use Mishusoft\Exceptions;
 use Mishusoft\MPM;
-use Mishusoft\Storage;
 use Mishusoft\Storage\FileSystem;
-use Mishusoft\System\Log;
 use Mishusoft\Utility\Debug;
 use Mishusoft\Utility\Implement;
 
@@ -15,45 +13,57 @@ class Common extends MPM
     /**
      * @return string
      */
-    public static function qualifiedAPIRoutesFile(): string
+    public static function httpAPIRoutesFile(): string
     {
-        return self::dFile(self::configDataFile('MPM', 'routes.api'));
+        return self::dFile(self::configDataFile('MPM', 'http.api'));
     }
 
 
     /**
      * @throws Exceptions\RuntimeException
      */
-    public static function autoUpdateQualifiedAPIRoutes(string $rootDirectory): void
+    public static function automatedUpdateHttpAPIRoutes(string $rootDirectory): void
     {
         $configs = [];
         if (count(FileSystem::list($rootDirectory, 'file')) > 0) {
             foreach (FileSystem::list($rootDirectory, 'file') as $filename) {
-                $filenameOriginal = $rootDirectory.$filename;
-                if (file_exists($filenameOriginal) && pathinfo($filenameOriginal, PATHINFO_EXTENSION) === 'json') {
-                    $configs[filemtime($filenameOriginal)] = (array) Implement::jsonDecode(
-                        FileSystem::read($filenameOriginal),
-                        IMPLEMENT_JSON_IN_ARR
-                    );
+                $filenameOriginal = $rootDirectory . $filename;
+                if (FileSystem::fileExt($filenameOriginal) === 'json') {
+                    $configs[FileSystem::fileName($filenameOriginal)] = [
+                        'lastModifiedAt' => filemtime($filenameOriginal),
+                        'configuration' => (array)Implement::jsonDecode(
+                            FileSystem::read($filenameOriginal),
+                            IMPLEMENT_JSON_IN_ARR
+                        )
+                    ];
                 }
             }
 
             array_multisort($configs, SORT_ASC);
             ksort($configs, SORT_ASC);
 
-            if (file_exists(self::qualifiedAPIRoutesFile()) === true) {
-                array_map(static function ($lastModifiedAt) use ($configs):void {
-                    if (filemtime(self::qualifiedAPIRoutesFile()) < $lastModifiedAt) {
-                        FileSystem::remove(self::qualifiedAPIRoutesFile());
-                        $configs = array_merge_recursive(
-                            FileSystem\Yaml::parseFile(self::qualifiedAPIRoutesFile()),
-                            array_values($configs)
-                        );
-                        FileSystem\Yaml::emitFile(self::qualifiedAPIRoutesFile(), array_values($configs));
-                    }
-                }, array_keys($configs));
+            if (file_exists(self::httpAPIRoutesFile()) === true) {
+                FileSystem\Yaml::emitFile(
+                    self::httpAPIRoutesFile(),
+                    array_merge_recursive(
+                        FileSystem\Yaml::parseFile(self::httpAPIRoutesFile()),
+                        array_map(static function ($details): array {
+                            if (filemtime(self::httpAPIRoutesFile()) < $details['lastModifiedAt']) {
+                                FileSystem::remove(self::httpAPIRoutesFile());
+                                return $details['configuration'];
+                            }
+                            return [];
+                        }, array_values($configs))
+                    )
+                );
             } else {
-                FileSystem\Yaml::emitFile(self::qualifiedAPIRoutesFile(), array_values($configs));
+                FileSystem\Yaml::emitFile(
+                    self::httpAPIRoutesFile(),
+                    array_map(
+                        static fn($details): array => $details['configuration'],
+                        array_values($configs)
+                    )
+                );
             }
         }
     }
