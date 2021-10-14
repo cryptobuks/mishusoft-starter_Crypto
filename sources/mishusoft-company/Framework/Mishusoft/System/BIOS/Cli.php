897dde242ce89db6adc45bee94c2c124f5a96592
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-15 05:05:21 +0600

    Create Cli.php

---
+++ sources/mishusoft-company/Framework/Mishusoft/System/BIOS/Cli.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mishusoft\System\BIOS;
+
+use Mishusoft\Cli\Request;
+use Mishusoft\System\BIOS;
+
+class Cli extends BIOS
+{
+    public static function initialise(): void
+    {
+        self::singleton(
+        /**
+         * @throws \Mishusoft\Exceptions\RuntimeException\NotFoundException
+         * @throws \Mishusoft\Exceptions\HttpException\HttpRequestException
+         */
+            function ($registry) {
+                $registry->requestCli = new Request();
+                \Mishusoft\Drivers\Bootstrap\Communication\Cli::run(new $registry->requestCli);
+            }
+        );
+    }
+}
