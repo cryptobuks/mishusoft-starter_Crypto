d896cb8792438e20d4c86e204b6c3cd1a157ba87
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-15 19:40:15 +0600

    Update Common.php

--- sources/mishusoft-company/Framework/Mishusoft/MPM/Common.php
+++ sources/mishusoft-company/Framework/Mishusoft/MPM/Common.php
@@ -5,7 +5,6 @@ namespace Mishusoft\MPM;
 use Mishusoft\Exceptions;
 use Mishusoft\MPM;
 use Mishusoft\Storage\FileSystem;
-use Mishusoft\Utility\Debug;
 use Mishusoft\Utility\Implement;
 
 class Common extends MPM
@@ -39,24 +38,31 @@ class Common extends MPM
                 }
             }
 
+            //sort configuration data
             array_multisort($configs, SORT_ASC);
             ksort($configs, SORT_ASC);
 
             if (file_exists(self::httpAPIRoutesFile()) === true) {
+                //Rewrite file with updated configuration
                 FileSystem\Yaml::emitFile(
                     self::httpAPIRoutesFile(),
-                    array_merge_recursive(
+                    array_filter(array_merge_recursive(
+                        //Read old data from exist file
                         FileSystem\Yaml::parseFile(self::httpAPIRoutesFile()),
                         array_map(static function ($details): array {
                             if (filemtime(self::httpAPIRoutesFile()) < $details['lastModifiedAt']) {
+                                //Remove file, if we need to rewrite file with updated configuration
                                 FileSystem::remove(self::httpAPIRoutesFile());
                                 return $details['configuration'];
                             }
                             return [];
                         }, array_values($configs))
-                    )
+                    ))
                 );
             } else {
+                //create new directory, if not found in file system
+                FileSystem::makeDirectory(dirname(self::httpAPIRoutesFile()));
+                //create new file with new configuration
                 FileSystem\Yaml::emitFile(
                     self::httpAPIRoutesFile(),
                     array_map(
