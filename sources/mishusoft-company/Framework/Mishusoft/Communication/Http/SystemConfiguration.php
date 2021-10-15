e22245daf20e53cf469d89453e2696d24808be24
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-15 19:40:05 +0600

    Update SystemConfiguration.php

--- sources/mishusoft-company/Framework/Mishusoft/Communication/Http/SystemConfiguration.php
+++ sources/mishusoft-company/Framework/Mishusoft/Communication/Http/SystemConfiguration.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Mishusoft\Communication;
+namespace Mishusoft\Communication\Http;
 
 use Mishusoft\Exceptions\LogicException\InvalidArgumentException;
 use Mishusoft\Exceptions\RuntimeException\NotFoundException;
