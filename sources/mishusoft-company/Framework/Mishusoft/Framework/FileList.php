c47964e9130231b480f2fa671d76343c673f75a7
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-17 01:34:40 +0600

    Create FileList.php

---
+++ sources/mishusoft-company/Framework/Mishusoft/Framework/FileList.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Mishusoft\Framework;
+
+use Mishusoft\Http;
+use Mishusoft\Storage;
+
+trait FileList
+{
+    /**
+     * @return string
+     */
+    public static function getAbsoluteInstalledURL(): string
+    {
+        return Http::getHost().Storage::applicationWebDirectivePath();
+    }//end getAbsoluteInstalledURL()
+
+
+    /**
+     * @return string
+     */
+    public static function configFile():string
+    {
+        return self::dFile(
+            self::configDataFile(
+                self::APP_TYPE,
+                'config'
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public static function installFile():string
+    {
+        return self::dFile(
+            self::configDataFile(
+                self::APP_TYPE,
+                'install'
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function listerFile():string
+    {
+        return self::dFile(
+            self::configDataFile(
+                self::APP_TYPE,
+                'files/' . APPLICATION_SERVER_NAME
+            ),
+            'ext4'
+        );
+    }
+}
