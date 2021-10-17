e31ac86762be695a7292a228432c881cc4ab701b
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-17 17:16:57 +0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,24 @@
 <?php
 
+/**
+ * The loader of http interface for mishusoft application
+ *
+ * Php version 7.4
+ *
+ * @category Loader
+ * @package  Mishusoft_Framework
+ * @author   Al-Amin Ahamed <alamin.rohita@hotmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://mishusoft.com
+ */
+
 //Load runtime defined ini file
-include_once __DIR__ . '/ini.php';
+require_once __DIR__ . '/ini.php';
 
 //Load pre defined constants
-include_once __DIR__ . '/constants.php';
-include_once __DIR__ . '/base64-files.php';
-include_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/constants.php';
+require_once __DIR__ . '/base64-files.php';
+require_once __DIR__ . '/functions.php';
 
 //Load mishusoft framework
-include_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/autoload.php';
