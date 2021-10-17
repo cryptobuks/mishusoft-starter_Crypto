49b33e746947e90a216b83bd58dfa76f1ed1e370
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-17 17:19:03 +0600

    Update functions.php

--- bootstrap/functions.php
+++ bootstrap/functions.php
@@ -1,15 +1,45 @@
 <?php
 
+/**
+ * The loader of global function for mishusoft application
+ *
+ * Php version 7.4
+ *
+ * @category Loader
+ * @package  Mishusoft_Framework
+ * @author   Al-Amin Ahamed <alamin.rohita@hotmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://mishusoft.com
+ */
+
+/**
+ * Make a error for flexible uses
+ *
+ * @param string $message     Text message about error
+ * @param int    $error_level Error type number
+ *
+ * @return void
+ */
 function error(string $message, int $error_level = E_USER_NOTICE)
 {
     $array = array_slice(debug_backtrace(), 1);
     $caller = array_shift($array);
     trigger_error(
-        sprintf('%1$s in %2$s on line %3$d', $message, $caller['file'], $caller['line']),
+        sprintf(
+            '%1$s in %2$s on line %3$d',
+            $message,
+            $caller['file'],
+            $caller['line']
+        ),
         $error_level
     );
 }
 
+/**
+ * Make a error for flexible uses
+ *
+ * @return string Return full path of framework
+ */
 function frameworkPath(): string
 {
     if (defined('FRAMEWORK_PATH') && file_exists(FRAMEWORK_PATH)) {
