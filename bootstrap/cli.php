6e5499e76dd14267b8110ad3e97663f804234418
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-17 17:18:54 +0600

    Update cli.php

--- bootstrap/cli.php
+++ bootstrap/cli.php
@@ -1,8 +1,20 @@
 <?php
 
+/**
+ * The loader of command line interface for mishusoft application
+ *
+ * Php version 7.4
+ *
+ * @category Loader
+ * @package  Mishusoft_Framework
+ * @author   Al-Amin Ahamed <alamin.rohita@hotmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://mishusoft.com
+ */
+
 //Load pre defined constants
-include_once __DIR__ . '/constants.php';
-include_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/constants.php';
+require_once __DIR__ . '/functions.php';
 
 //Load mishusoft framework
-include_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/autoload.php';
