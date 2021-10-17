f8783cf0192c5869aeed212cd4d9f620b5564b57
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-17 17:17:05 +0600

    Update autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * The loader of mishusoft application
+ *
+ * Php version 7.4
+ *
+ * @category Loader
+ * @package  Mishusoft_Framework
+ * @author   Al-Amin Ahamed <alamin.rohita@hotmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://mishusoft.com
+ */
+
 // Set customize error controller.
 
 use Mishusoft\Exceptions\Handler;
@@ -7,7 +19,13 @@ use Mishusoft\Exceptions\Handler;
 set_error_handler(
     static function ($number, $message, $file, $line) {
         //print_r(debug_backtrace(), false);
-        Handler::fetchError($number, $message, $file, $line, array_slice(debug_backtrace(), 1));
+        Handler::fetchError(
+            $number,
+            $message,
+            $file,
+            $line,
+            array_slice(debug_backtrace(), 1)
+        );
     },
     E_ALL
 );
@@ -21,36 +39,35 @@ set_exception_handler(
 
 /**
  * Automatically load all required classes.
- *
  */
 spl_autoload_register(
     static function (string $class) {
-        if (strncmp($class, WHO_AM_I, strlen(WHO_AM_I)) !== 0) {
-            $class = sprintf('%1$s\\%2$s', WHO_AM_I, $class);
+        if (strncmp($class, \WHO_AM_I, strlen(\WHO_AM_I)) !== 0) {
+            $class = sprintf('%1$s\\%2$s', \WHO_AM_I, $class);
         }
-
-        //print_r(' SYS PATH : ' . $frameworkPath . '<br/>', false);
-        //print_r(' REQUIRED : ' . $class . '<br/>', false);
-        //print_r('<br/>', false);
 
         // Check file is use namespace.
         if (is_int(strpos($class, '\\'))) {
             // Extract file namespace to file location.
-            $originalFile = frameworkPath() . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+            $originalFile = sprintf(
+                '%1$s%2$s.php',
+                frameworkPath(),
+                str_replace('\\', DIRECTORY_SEPARATOR, $class)
+            );
 
-            //print_r(' PHYSICAL FILE : ' . $originalFile . '<br/>', false);
-            //print_r('<br/>', false);
-            // Checking local file: $originalFile.
             if (is_file($originalFile)) {
-                // print_r(' PHYSICAL FILE IS FOUND<br/>', false);
-                // print_r('<br/>', false);
-                // print_r('<br/>', false);
                 include_once $originalFile;
             }
         } else {
             // Want to load normal File $class.
             foreach (scandir(realpath(dirname(__FILE__, 2))) as $directory) {
-                $originalFile = frameworkPath() . ucfirst($directory) . DS . ucfirst($class) . '.php';
+                $originalFile = sprintf(
+                    '%1$s%2$s%4$s%3$s.php',
+                    frameworkPath(),
+                    ucfirst($directory),
+                    ucfirst($class),
+                    DS
+                );
                 if (is_file($originalFile)) {
                     include_once $originalFile;
                 }
