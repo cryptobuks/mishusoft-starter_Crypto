79c7c7155029d2ad00757c3fccb1ae9e8555accb
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-24 14:00:10 +0600

    improve(nodejs): creating new implement for nodejs

--- jobs/nodejs-test/scandir.js
+++ jobs/nodejs-test/scandir.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs").promises;
 
-(async function (path) {
+/*(async function (path) {
   try {
     await fs.unlink(path);
     console.log(`successfully deleted ${path}`);
@@ -9,3 +9,18 @@ const fs = require("fs").promises;
     console.error("there was an error:", error.message);
   }
 })(path.resolve(__dirname, "./tmp/hello"));
+
+const fs = require("fs").promises;*/
+
+(async function (from, to) {
+  try {
+    await fs.rename(from, to);
+    const stats = await fs.stat(to);
+    console.log(`stats: ${JSON.stringify(stats)}`);
+  } catch (error) {
+    console.error("there was an error:", error.message);
+  }
+})(
+  path.resolve(__dirname, "./tmp/hello"),
+  path.resolve(__dirname, "./tmp/world")
+);
