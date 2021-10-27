718529fb8c848c2558396a8eb745503eba7725bc
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-27 16:06:26 +0600

    Update installer.ts

--- sources/Assets/typescripts/installer.ts
+++ sources/Assets/typescripts/installer.ts
@@ -1,19 +1,24 @@
-import('./common/dom').then(function (dom) {
+import("./common/dom")
+  .then(function (dom) {
     let captureElement = dom.captureElement;
     ((command: string): void => {
-        if (command === 'install') {
-            import('./db/app').then(function (db) {
-                import('./mishusoft/Installer').then(function (t) {
-                    new t.MishusoftInstaller(db.appHost, db.app).play();
-
-                }).catch(function (err) {
-                    console.log(err)
-                })
-            }).catch(function (err) {
-                console.log(err)
-            });
-        }
-    })(captureElement('body').id);
-}).catch(function (err) {
-    console.log(err)
-});
+      if (command === "install") {
+        import("./db/app")
+          .then(function (db) {
+            import("./mishusoft/Installer")
+              .then(function (t) {
+                new t.MishusoftInstaller(db.appHost, db.app).play();
+              })
+              .catch(function (err) {
+                console.log(err);
+              });
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      }
+    })(captureElement("body").id);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
