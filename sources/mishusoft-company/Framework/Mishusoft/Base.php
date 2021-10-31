699feb96c04040bdf0ba4720e156663b74b06393
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-31 07:49:26 +0600

    Update Base.php

--- sources/mishusoft-company/Framework/Mishusoft/Base.php
+++ sources/mishusoft-company/Framework/Mishusoft/Base.php
@@ -6,41 +6,35 @@ use Mishusoft\Utility\Inflect;
 
 abstract class Base extends Singleton
 {
-    public const SYSTEM_APP_FILE    = 'php';
-    public const SYSTEM_DATA_FILE   = 'yml';
-    public const PUBLIC_DATA_FILE   = 'json';
+    public const SYSTEM_APP_FILE = "php";
+    public const SYSTEM_DATA_FILE = "yml";
+    public const PUBLIC_DATA_FILE = "json";
 
+    //Basic functionalities
 
-    protected static function dataFileFormat(): string
-    {
-        return 'yml';
-    }
-
-
-    protected static function dFile(string $path, string $extension = 'yml'): string
-    {
-        return sprintf('%s.%s', $path, $extension);
-    }
-
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
+    /**
+     * @param string $string
+     * @return string
+     */
     public static function hidePath(string $string): string
     {
         return str_replace(RUNTIME_ROOT_PATH, ROOT_IDENTITY, $string);
     }
 
+    /**
+     * @param string $filename
+     * @return string
+     */
     public static function getClassNamespace(string $filename): string
     {
         //make namespace from source path (if development, app://sources/company/Framework)
         if (Inflect::startsWith($filename, RUNTIME_SOURCES_PATH)) {
-            return self::namespaceBuilder(RUNTIME_SOURCES_PATH . 'Framework' . DS, $filename);
+            return self::namespaceBuilder(RUNTIME_SOURCES_PATH . "Framework" . DS, $filename);
         }
 
         //make namespace from (app://Framework) production
-        return self::namespaceBuilder(RUNTIME_ROOT_PATH . 'Framework' . DS, $filename);
-    }//end getClassNamespace()
+        return self::namespaceBuilder(RUNTIME_ROOT_PATH . "Framework" . DS, $filename);
+    }
 
     private static function namespaceBuilder(string $rootPath, string $filename): string
     {
@@ -48,449 +42,340 @@ abstract class Base extends Singleton
         return self::makePathToNamespace($filename);
     }
 
-<<<<<<< Updated upstream
-    private static function makePathToNamespace(string $path): string
-=======
+    /**
+     * @param string $resources
+     * @param string $path
+     * @return int
+     */
     private static function startPosition(string $resources, string $path): int
->>>>>>> Stashed changes
-    {
-        return str_replace(['//', '/'], ['/', '\\'], substr($path, 0, self::lastPosition($path)));
-    }
-
-<<<<<<< Updated upstream
-    private static function startPosition(string $resources, string $path): string
-=======
-    private static function makePathToNamespace(string $path): string
->>>>>>> Stashed changes
     {
         $path = rtrim($path, DS);
         //get actual point from path/name/with/file.extension
-        return (strpos($resources, $path) + strlen($path));
+        return strpos($resources, $path) + strlen($path);
     }
 
-<<<<<<< Updated upstream
-    private static function lastPosition(string $path): string
-=======
-    private static function lastPosition(string $path): int
->>>>>>> Stashed changes
+    /**
+     * @param string $path
+     * @return string
+     */
+    private static function makePathToNamespace(string $path): string
     {
-        return (strlen($path) - (strlen($path) - strpos($path, '.php')));
+        return str_replace(["//", "/"], ["/", "\\"], substr($path, 0, self::lastPosition($path)));
     }
 
+    /**
+     * @param string $path
+     * @return int
+     */
+    private static function lastPosition(string $path): int
+    {
+        return strlen($path) - (strlen($path) - strpos($path, ".php"));
+    }
 
     /**
      * Get path from psr_4 namespace
+     *
+     * @param string $name_space
+     * @param string $extension
+     * @return string
      */
-    public static function getPath(string $name_space, string $extension = '.php'): string
+    public static function getPath(string $name_space, string $extension = ".php"): string
     {
-        $file = str_replace('\\', DS, $name_space).$extension;
-        return RUNTIME_ROOT_PATH.$file;
-    }//end getPath()
+        $file = str_replace("\\", DS, $name_space) . $extension;
+        return RUNTIME_ROOT_PATH . $file;
+    }
+
     /**
      * Get current directory name from full path
+     *
+     * @param string $fullPath
+     * @param string $rootPath
+     * @return string
      */
     public static function getDirectoryName(string $fullPath, string $rootPath = RUNTIME_ROOT_PATH): string
     {
-<<<<<<< Updated upstream
-        return strtolower(
-            substr(
-                $fullPath,
-                strlen($rootPath),
-                ((strlen($fullPath) - strlen($rootPath)) - 1)
-            )
-        );
-    }public static function rootPath(): string
-=======
         return strtolower(substr($fullPath, strlen($rootPath), strlen($fullPath) - strlen($rootPath) - 1));
     }
 
+    /**
+     * @return string
+     */
     public static function frameworkViewsPath(): string
->>>>>>> Stashed changes
     {
-        return RUNTIME_ROOT_PATH;
+        return sprintf('%1$s%2$s%3$s', self::frameworkDataPath(), "views", DS);
     }
 
-    public static function storagesPath(): string
+    protected static function frameworkDataPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            RUNTIME_ROOT_PATH,
-            'storages',
-            DS
-        );
+        return sprintf('%1$s%2$s%4$s%3$s%4$s', self::rootPath(), "storages", "framework", DS);
     }
 
-<<<<<<< Updated upstream
+    //End basic functionalities
 
-    public static function appStoragesPath(): string
-=======
+    //Path making functionalities
+
+    /**
+     * @return string
+     */
     public static function rootPath(): string
     {
         return RUNTIME_ROOT_PATH;
     }
 
+    /**
+     * @return string
+     */
     public static function cssAssetsPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::storagesPath(),
-            'app',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::assetsPath(), "css", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function assetsPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::appStoragesPath(),
-            'assets',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::appStoragesPath(), "assets", DS);
     }
 
-<<<<<<< Updated upstream
-    public static function frameworkViewsPath(): string
-=======
+    /**
+     * @return string
+     */
     public static function appStoragesPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::frameworkDataPath(),
-            'views',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::storagesPath(), "app", DS);
     }
 
-<<<<<<< Updated upstream
-    public static function cssAssetsPath(): string
-=======
+    /**
+     * @return string
+     */
     public static function storagesPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::assetsPath(),
-            'css',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', RUNTIME_ROOT_PATH, "storages", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function jsAssetsPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::assetsPath(),
-            'js',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::assetsPath(), "js", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function webfontsAssetsPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::assetsPath(),
-            'webfonts',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::assetsPath(), "webfonts", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function localizationPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::appStoragesPath(),
-            'localization',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::appStoragesPath(), "localization", DS);
     }
 
-<<<<<<< Updated upstream
-    public static function mediaPath(): string
-=======
+    /**
+     * @return string
+     */
     public static function imagesPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::appStoragesPath(),
-            'media',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::mediaPath(), "images", DS);
     }
 
-<<<<<<< Updated upstream
-    public static function imagesPath(): string
-=======
+    /**
+     * @return string
+     */
     public static function mediaPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::mediaPath(),
-            'images',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::appStoragesPath(), "media", DS);
     }
 
-<<<<<<< Updated upstream
-    protected static function frameworkDataPath(): string
-=======
+    /**
+     * @return string
+     */
     public static function frameworkStoragesPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%4$s%3$s%4$s',
-            self::rootPath(),
-            'storages',
-            'framework',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::storagesPath(), "framework", DS);
     }
 
-<<<<<<< Updated upstream
-
-    public static function frameworkStoragesPath(): string
-=======
+    /**
+     * @param bool $isRemote
+     * @return string
+     */
     public static function logosDefaultPath(bool $isRemote = false): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::storagesPath(),
-            'framework',
-            DS
-        );
+        return sprintf('%1$s%2$s', self::logosPath($isRemote), "default/");
     }
 
-<<<<<<< Updated upstream
-
-
-=======
->>>>>>> Stashed changes
+    /**
+     * @param bool $isRemote
+     * @return string
+     */
     public static function logosPath(bool $isRemote = false): string
     {
         if ($isRemote) {
-            return sprintf(
-                '%1$s%2$s',
-                BASE_URL,
-                'media/logos/'
-            );
+            return sprintf('%1$s%2$s', BASE_URL, "media/logos/");
         }
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::mediaPath(),
-            'logos',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::mediaPath(), "logos", DS);
     }
 
-<<<<<<< Updated upstream
-    public static function logosDefaultPath(bool $isRemote = false): string
-=======
+    /**
+     * @return string
+     */
     public static function uploadsPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s',
-            self::logosPath($isRemote),
-            'default/'
-        );
+        return sprintf('%1$s%2$s%3$s', self::mediaPath(), "uploads", DS);
     }
 
-<<<<<<< Updated upstream
-    public static function uploadsPath(): string
-=======
+    /**
+     * @return string
+     */
     public static function usersProfilePicturesPath(): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::mediaPath(),
-            'uploads',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::usersPicturesPath(), "profiles", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function usersPicturesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::mediaPath(),
-            'users',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::mediaPath(), "users", DS);
     }
 
-    public static function usersProfilePicturesPath(): string
-    {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::usersPicturesPath(),
-            'profiles',
-            DS
-        );
-    }
-
+    /**
+     * @return string
+     */
     public static function usersCoverPicturesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::usersPicturesPath(),
-            'covers',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::usersPicturesPath(), "covers", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function usersBackgroundPicturesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::usersPicturesPath(),
-            'backgrounds',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::usersPicturesPath(), "backgrounds", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function databasesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::storagesPath(),
-            'databases',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::storagesPath(), "databases", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function cachesStoragesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::frameworkDataPath(),
-            'caches',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::frameworkDataPath(), "caches", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function logsStoragesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::frameworkDataPath(),
-            'logs',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::frameworkDataPath(), "logs", DS);
     }
 
+    /**
+     * @return string
+     */
     public static function dataDriveStoragesPath(): string
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::frameworkDataPath(),
-            'data-drive',
-            DS
-        );
+        return sprintf('%1$s%2$s%3$s', self::frameworkDataPath(), "data-drive", DS);
     }
 
+    /**
+     * @param string $directive
+     * @return string
+     */
     public static function configDataDirective(string $directive): string
     {
-        return sprintf(
-            '%1$s%2$s%4$s%3$s',
-            self::frameworkDataPath(),
-            'configs',
-            $directive,
-            DS
-        );
+        return sprintf('%1$s%2$s%4$s%3$s', self::frameworkDataPath(), "configs", $directive, DS);
     }
 
-<<<<<<< Updated upstream
-
-    protected static function logDirective(string $directive): string
-=======
+    /**
+     * @return string
+     */
     public static function frameworkSessionsPath(): string
     {
         return sprintf('%1$s%2$s%3$s', self::frameworkDataPath(), "sessions", DS);
     }
 
+    /**
+     * @return string
+     */
     protected static function dataFileFormat(): string
     {
         return "yml";
     }
 
+    /**
+     * @param string $path
+     * @param string $extension
+     * @return string
+     */
     protected static function dFile(string $path, string $extension = "yml"): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%4$s%3$s%4$s',
-            self::frameworkDataPath(),
-            'logs',
-            $directive,
-            DS
-        );
+        return sprintf("%s.%s", $path, $extension);
     }
 
-<<<<<<< Updated upstream
-    public static function frameworkSessionsPath(): string
-=======
+    /**
+     * @param string $directive
+     * @return string
+     */
     protected static function logDirective(string $directive): string
->>>>>>> Stashed changes
     {
-        return sprintf(
-            '%1$s%2$s%3$s',
-            self::frameworkDataPath(),
-            'sessions',
-            DS
-        );
+        return sprintf('%1$s%2$s%4$s%3$s%4$s', self::frameworkDataPath(), "logs", $directive, DS);
     }
 
+    /**
+     * @param string $directive
+     * @param string $filename
+     * @return string
+     */
     protected static function cacheDataFile(string $directive, string $filename): string
     {
-        return sprintf(
-            '%1$s%2$s%5$s%3$s%5$s%4$s',
-            self::frameworkDataPath(),
-            'caches',
-            $directive,
-            $filename,
-            DS
-        );
+        return sprintf('%1$s%2$s%5$s%3$s%5$s%4$s', self::frameworkDataPath(), "caches", $directive, $filename, DS);
     }
 
+    /**
+     * @param string $directive
+     * @param string $filename
+     * @return string
+     */
     protected static function configDataFile(string $directive, string $filename): string
     {
-        return sprintf(
-            '%1$s%2$s%5$s%3$s%5$s%4$s',
-            self::frameworkDataPath(),
-            'configs',
-            $directive,
-            $filename,
-            DS
-        );
+        return sprintf('%1$s%2$s%5$s%3$s%5$s%4$s', self::frameworkDataPath(), "configs", $directive, $filename, DS);
     }
 
+    /**
+     * @param string $directive
+     * @param string $filename
+     * @return string
+     */
     protected static function requiredDataFile(string $directive, string $filename): string
     {
-        return sprintf(
-            '%1$s%2$s%5$s%3$s%5$s%4$s',
-            self::frameworkDataPath(),
-            'data-drive',
-            $directive,
-            $filename,
-            DS
-        );
+        return sprintf('%1$s%2$s%5$s%3$s%5$s%4$s', self::frameworkDataPath(), "data-drive", $directive, $filename, DS);
     }
 
+    /**
+     * @param string $directive
+     * @param string $filename
+     * @return string
+     */
     protected static function logDataFile(string $directive, string $filename): string
     {
-        return sprintf(
-            '%1$s%2$s%5$s%3$s%5$s%4$s',
-            self::frameworkDataPath(),
-            'logs',
-            $directive,
-            $filename,
-            DS
-        );
+        return sprintf('%1$s%2$s%5$s%3$s%5$s%4$s', self::frameworkDataPath(), "logs", $directive, $filename, DS);
     }
 }
